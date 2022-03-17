196ddef17497a52441b274ac06f8ed6d27e50e8d
JodelleMarx <101527787+JodelleMarx@users.noreply.github.com>
2022-03-17 08:55:13 -0700

    Added weather icon that correlates with search input

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-function showTemp(response) {
-  let cityDisplay = document.querySelector("#city-display");
-  cityDisplay.innerHTML = response.data.name;
-  let celsius = document.querySelector("#celsius");
-  let fahrenheit = document.querySelector("#fahrenheit");
-  let sourcedTemp = Math.round(response.data.main.temp);
-  let temp = document.querySelector("#temp");
-  let weatherDescription = document.querySelector("#weather-description");
-  celsiusTemp = response.data.main.temp;
-  temp.innerHTML = ` ${sourcedTemp}°C`;
-  celsius.innerHTML = `°C |`;
-  fahrenheit.innerHTML = ` °F`;
-  weatherDescription.innerHTML = `Today's forecast: ${response.data.weather[0].description}.`;
-}
-
-function searchCityWeather(cityName) {
-  let apiKey = "8f74c72399c350e75af3f25df3b5a966";
-  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
-  axios
-    .get(`${apiUrl}q=${cityName}&appid=${apiKey}&units=metric`)
-    .then(showTemp);
-}
-
 function searchCity(event) {
   formatDateTime();
   event.preventDefault();
@@ -34,8 +11,35 @@ function searchCity(event) {
   }
 }
 
-let searchEngine = document.querySelector("#search-engine");
-searchEngine.addEventListener("submit", searchCity);
+function showTemp(response) {
+  let cityDisplay = document.querySelector("#city-display");
+  cityDisplay.innerHTML = response.data.name;
+  let celsius = document.querySelector("#celsius");
+  let fahrenheit = document.querySelector("#fahrenheit");
+  let sourcedTemp = Math.round(response.data.main.temp);
+  let temp = document.querySelector("#temp");
+  let weatherDescription = document.querySelector("#weather-description");
+  let icon = document.querySelector("#icon");
+  let iconCode = response.data.weather[0].icon;
+
+  celsiusTemp = response.data.main.temp;
+  temp.innerHTML = ` ${sourcedTemp}°C`;
+  celsius.innerHTML = `°C |`;
+  fahrenheit.innerHTML = ` °F`;
+  weatherDescription.innerHTML = `Today's forecast: ${response.data.weather[0].description}.`;
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+  );
+}
+
+function searchCityWeather(cityName) {
+  let apiKey = "8f74c72399c350e75af3f25df3b5a966";
+  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+  axios
+    .get(`${apiUrl}q=${cityName}&appid=${apiKey}&units=metric`)
+    .then(showTemp);
+}
 
 function formatDateTime(timestamp) {
   let now = new Date();
@@ -96,6 +100,9 @@ function displayCelsiusTemp(event) {
   celsiusLink.classList.add("active");
   temp.innerHTML = ` ${Math.round(celsiusTemp)}°C`;
 }
+
+let searchEngine = document.querySelector("#search-engine");
+searchEngine.addEventListener("submit", searchCity);
 
 let celsiusTemp = null;
 
