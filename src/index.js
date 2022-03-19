cbb1da80055eaf2c9c5437a0bec4b99acc37efad
JodelleMarx <101527787+JodelleMarx@users.noreply.github.com>
2022-03-19 14:59:01 -0700

    Added API call for forecast

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ function searchCity(event) {
   }
 }
 
+function getForecast(coordinates) {
+  let apiKey = "8f74c72399c350e75af3f25df3b5a966";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
+}
+
 function showTemp(response) {
   let cityDisplay = document.querySelector("#city-display");
   cityDisplay.innerHTML = response.data.name;
@@ -34,6 +40,8 @@ function showTemp(response) {
     "src",
     `http://openweathermap.org/img/wn/${iconCode}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function searchCityWeather(cityName) {
@@ -43,18 +51,6 @@ function searchCityWeather(cityName) {
     .get(`${apiUrl}q=${cityName}&appid=${apiKey}&units=metric`)
     .then(showTemp);
 }
-
-// function showPosition(position) {
-//   let cityDisplay = document.querySelector("cityDisplay");
-//   cityDisplay.innerHTML = `Your Latitude is ${position.coords.latitude} and your longitude is ${position.coords.longitude}.`;
-// }
-
-// function getCurrentLocation() {
-//   navigator.geolocation.getCurrentPosition(showPosition);
-// }
-
-// let myLocationBtn = document.querySelector("button");
-// GamepadButton.addEventListener("click", getCurrentPosition);
 
 function formatDateTime(timestamp) {
   let now = new Date();
@@ -118,7 +114,8 @@ function displayCelsiusTemp(event) {
   temp.innerHTML = ` ${Math.round(celsiusTemp)}°C`;
 }
 
-function dispalyForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecast = document.querySelector("#weather-forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -155,5 +152,3 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
-
-dispalyForecast();
